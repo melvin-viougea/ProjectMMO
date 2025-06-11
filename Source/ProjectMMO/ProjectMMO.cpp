0c62e8f9e42afb5bf4e2abec1c65784b495d07e1
Melvin Viougea <mviougea@icloud.com>
2025-06-11 19:47:32 +0200

    init

---
+++ Source/ProjectMMO/ProjectMMO.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectMMO.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectMMO, "ProjectMMO" );
